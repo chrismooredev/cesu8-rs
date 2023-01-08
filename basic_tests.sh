830808069089cbcf27c6f721c1ad189c4cee370a
Chris Moore <git@chrismoore.dev>
2023-01-08 01:36:41 -0600

    better label utf8 random texts

--- basic_tests.sh
+++ basic_tests.sh
@@ -12,10 +12,10 @@ cargo test
 echo "testing group (random.txt)..."
 
 echo "xxd -r random.txt.xxd"
-cmp --quiet random.txt <(xxd -r random.txt.xxd)
+cmp --quiet random.utf8.txt <(xxd -r random.utf8.txt.xxd)
 
 echo "cesu8 --decode random.cesu8.txt"
-cmp --quiet random.txt <(./target/debug/cesu8 --decode random.cesu8.txt)
+cmp --quiet random.utf8.txt <(./target/debug/cesu8 --decode random.cesu8.txt)
 
 echo "testing group random.cesu8.txt"
 
@@ -23,9 +23,9 @@ echo "xxd -r random.cesu8.txt.xxd"
 cmp --quiet random.cesu8.txt <(xxd -r random.cesu8.txt.xxd)
 
 echo "cesu8 random.txt"
-cmp --quiet random.cesu8.txt <(./target/debug/cesu8 random.txt)
+cmp --quiet random.cesu8.txt <(./target/debug/cesu8 random.utf8.txt)
 
 echo "round trip `cesu8 random.txt | cesu8 --decode`"
-cmp --quiet random.txt <(./target/debug/cesu8 random.txt | ./target/debug/cesu8 --decode)
+cmp --quiet random.utf8.txt <(./target/debug/cesu8 random.utf8.txt | ./target/debug/cesu8 --decode)
 
 echo "all tests successful"
